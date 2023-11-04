be8ffc68fb64a8b0cc0c6387ed9171f23bac2d45
kawasaki-yuto <y.baseball.8796@gmail.com>
2023-11-04 15:36:21 +0900

    settings.py 修正

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
-
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,3 +122,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+import django_heroku
+
+django_heroku.settings(locals())
