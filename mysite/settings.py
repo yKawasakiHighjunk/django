338435012b37c7af6328c3661fe3a94c0fe52012
kawasaki-yuto <y.baseball.8796@gmail.com>
2023-11-04 21:42:56 +0900

    db

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,15 +74,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "mysite.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+import dj_database_url
+from dotenv import (
+    find_dotenv,
+    load_dotenv,
+)
+load_dotenv(find_dotenv())
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(conn_max_age=600),
 }
 
 
